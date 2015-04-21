36a9d098363cc963e5d41234116f0284c5539e02
steven <rendong_liu@hotmail.com>
2015-04-21 23:49:02 +0200

    add the graphic thread

--- app/src/main/java/com/example/steven/app1/GFX.java
+++ app/src/main/java/com/example/steven/app1/GFX.java
@@ -1,7 +1,9 @@
 package com.example.steven.app1;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 
 /**
  * Created by steven on 20-4-15.
@@ -9,14 +11,27 @@ import android.os.Bundle;
 public class GFX extends Activity {
 
     MyBringBack outVeiw;
+    PowerManager.WakeLock wl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        PowerManager pm=(PowerManager)getSystemService(Context.POWER_SERVICE);
+         wl=pm.newWakeLock(PowerManager.RELEASE_FLAG_WAIT_FOR_NO_PROXIMITY,"hi");
 
+        super.onCreate(savedInstanceState);
+        wl.acquire();
         outVeiw =new MyBringBack(getApplicationContext());
         setContentView(outVeiw);
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        wl.release();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 }
--- app/src/main/java/com/example/steven/app1/Surface.java
+++ app/src/main/java/com/example/steven/app1/Surface.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Picture;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,15 +19,30 @@ import android.view.View;
 public class Surface extends Activity implements View.OnTouchListener{
 
     MyBringBackSurface surfaceview;
-    float x,y;
+    float x,y,sX,sY,fX,fY;
+    float dX,dY,aniX,aniY;
+    float scaledX=0;
+    float scaledY=0;
+    Bitmap test;
+    Bitmap background;
+    Bitmap plus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         surfaceview = new MyBringBackSurface(this);
         surfaceview.setOnTouchListener(this);
+        test=BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
+        background= BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        plus=BitmapFactory.decodeResource(getResources(),R.drawable.pos);
         x=0;
         y=0;
+        sX=0;
+        sY=0;
+        fX=0;
+        fY=0;
+        dX=dY=0;
+        aniX=aniY=0;
         setContentView(this.surfaceview);
     }
 
@@ -44,10 +60,55 @@ public class Surface extends Activity implements View.OnTouchListener{
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+
+        /*
+            FPS , to lower the FPS, use sleep. Maybe not a good option;
+         */
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         x=event.getX();
         y=event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+            {
+                dX=0;
+                dY=0;
+                fX=0;
+                fY=0;
+                aniX=0;
+                aniY=0;
+                scaledX=0;
+                scaledY=0;
+                sX=event.getX();
+                sY=event.getY();
+
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            {
+                fX=event.getX();
+                fY=event.getY();
+                dX=fX-sX;
+                dY=fX-sY;
+                scaledX=dX/30;
+                scaledY=dY/30;
+                x=0;
+                y=0;
+
+                break;
+            }
+
+
+
+
+        }
         Log.e( this.getClass().getSimpleName(),"x:  "+x+"----"+"y:  "+y);
-        return false;
+        return true;
     }
 
 
@@ -74,11 +135,24 @@ public class Surface extends Activity implements View.OnTouchListener{
                     continue;
                 }
                 Canvas canvas = ourHolder.lockCanvas();
-                canvas.drawRGB(21,241,120);
+                canvas.drawBitmap(background,0,0,null);
+//                canvas.drawRGB(21,241,120);
                 if( x!=0 && y!=0){
-                    Bitmap test= BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
-                    canvas.drawBitmap(test, x,y, null);
+
+                    canvas.drawBitmap(test, x-test.getWidth()/2,y-test.getHeight()/2, null);
                 }
+                if( sX!=0 && sY!=0){
+
+                    canvas.drawBitmap(plus, sX-test.getWidth()/2,sY-test.getHeight()/2, null);
+                }
+                if( fX!=0 && fY!=0){
+
+                    canvas.drawBitmap(test, fX-test.getWidth()/2-aniX,fY-test.getHeight()/2-aniY, null);
+                    canvas.drawBitmap(plus, fX-test.getWidth()/2,fY-test.getHeight()/2, null);
+
+                }
+                aniX=aniX+scaledX;
+                aniY=aniY+scaledY;
                 ourHolder.unlockCanvasAndPost(canvas);
             }
         }
