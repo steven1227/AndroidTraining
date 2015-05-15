db83d3bbb87e4a4347969db56ae6f7978aca35d2
steven <rendong_liu@hotmail.com>
2015-05-15 13:15:52 +0200

    fix the button

--- app/src/main/java/com/example/steven/app1/Draw2.java
+++ app/src/main/java/com/example/steven/app1/Draw2.java
@@ -1,8 +1,12 @@
 package com.example.steven.app1;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,11 +18,12 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 
-public class Draw2 extends ActionBarActivity implements AdapterView.OnItemClickListener {
+public class Draw2 extends Activity implements AdapterView.OnItemClickListener {
 
     private DrawerLayout drawerLayout;
     private ListView listView;
     private ArrayList<String> planets;
+    private ActionBarDrawerToggle drawlisten;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +31,40 @@ public class Draw2 extends ActionBarActivity implements AdapterView.OnItemClickL
         setContentView(R.layout.activity_draw2);
         listView=(ListView)findViewById(R.id.drawerlist);
         planets=new ArrayList<>();
-        listView.setAdapter(new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1,planets));
+        listView.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, planets));
         listView.setOnItemClickListener(this);
         planets.add("Welcome");
         planets.add("quit");
+
+
+        this.drawerLayout=(DrawerLayout)findViewById(R.id.drawerLayout);
+        drawlisten=new ActionBarDrawerToggle(this,this.drawerLayout,R.drawable.ic_launcher,R.drawable.ic_launcher_web){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                Toast.makeText(getApplicationContext(),"open",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                Toast.makeText(getApplicationContext(),"close",Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+        drawerLayout.setDrawerListener(this.drawlisten);
+        ActionBar bar=getActionBar();
+        bar.setHomeButtonEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.drawlisten.syncState();
     }
 
     @Override
@@ -44,6 +79,8 @@ public class Draw2 extends ActionBarActivity implements AdapterView.OnItemClickL
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        drawlisten.onOptionsItemSelected(item);
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -55,8 +92,14 @@ public class Draw2 extends ActionBarActivity implements AdapterView.OnItemClickL
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        this.drawlisten.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(this,planets.get(position),Toast.LENGTH_SHORT).show();
-        getSupportActionBar().setTitle(planets.get(position));
+        getActionBar().setTitle(planets.get(position));
     }
 }
